be0fdd4b9f9d93f809df5e6d00bf8c83e43703be
Aleksey Paramonov <paramonovav90@gmail.com>
2022-02-23 20:29:55 +0400

    70% form

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 let popup = document.querySelector(".popup");
+let reductButton = document.querySelector(".info__button-reduct");
+let popupItem = popup.querySelectorAll(".popup__item");
+let profileName = document.querySelector(".info__name");
+let profileAbout = document.querySelector(".info__about");
+let saveReduct = popup.querySelector(".popup__button-save");
+let buttonCloseReduct = popup.querySelector(".popup__button-close");
 
-
-console.log(popup);
-let reduct = document.querySelector(".info__button-reduct");
-console.log(reduct);
-let item = popup.querySelectorAll(".popup__item");
-console.log(item);
-let save = popup.querySelector(".popup__button-save");
-console.log(save);
-let close = popup.querySelector(".popup__button-close");
-console.log(close);
-function openRedact(){
-  popup.setAttribute("class", "popup popup_opened");
+function openOrCloseReduct(){
+  popup.classList.toggle("popup_opened");
   return popup;
 }
-reduct.addEventListener('hover', function openRedact(){
+
+reductButton.addEventListener('click', function(){
+  popup = openOrCloseReduct();
+  popupItem[0].textContent = profileName.textContent;
+  popupItem[1].textContent = profileAbout.textContent;
+  console.log(popupItem[0].textContent);
+  console.log(profileName.textContent);
+});
+
+buttonCloseReduct.addEventListener('click', function(){
+  popup = openOrCloseReduct();
+  console.log(popupItem[0].textContent);
+  console.log(profileName.textContent);
+});
+
+
+
+saveReduct.addEventListener('click', function(evt){
+  evt.preventDefault();
+  profileName.textContent = popupItem[0].textContent;
+  profileAbout.textContent = popupItem[1].textContent;
+  popup = openOrCloseReduct();
+  console.log(popupItem[0].textContent);
+  console.log(profileName.textContent);
 });
 
